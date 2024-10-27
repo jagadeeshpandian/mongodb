806e33a980437370a457bb7ceee710205bc13f1b
Jagadeesh P <jagadeeshprofile@yahoo.in>
2024-10-27 22:49:03 +0530

    First Mongidb file
    This file contains script to insertOne and insertMany into Users collection

---
+++ Mongodb_insert.js
@@ -0,0 +1,17 @@
+{
+  "acknowledged": true,
+  "insertedIds": {
+    "0": {
+      "$oid": "671e74730cac4b4210829ead"
+    },
+    "1": {
+      "$oid": "671e74730cac4b4210829eae"
+    },
+    "2": {
+      "$oid": "671e74730cac4b4210829eaf"
+    },
+    "3": {
+      "$oid": "671e74730cac4b4210829eb0"
+    }
+  }
+}
